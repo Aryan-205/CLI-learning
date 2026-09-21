ce337f6d97f263f75ecd32db75cd18a1585835ba
Aryan Bola <aaryann5002@gmail.com>
2026-09-21 13:47:34 +0530

    making my first cli tool

---
+++ tool/src/commands/config-mgr.js
@@ -0,0 +1,23 @@
+const chalk = require('chalk').default;
+const { cosmiconfigSync } = require('cosmiconfig');
+const schema = require('../config/schema.json');
+const Ajv = require('ajv').default;
+const ajv = new Ajv();
+const configLoader = cosmiconfigSync('tool');
+
+module.exports = function getConfig() {
+  const result = configLoader.search(process.cwd());
+  if (!result) {
+    console.log(chalk.yellow('Could not find configuration, using default'));
+    return { port: 1234 };
+  } else {
+    const isValid = ajv.validate(schema, result.config);
+    if (!isValid) {
+      console.log(chalk.yellow('Invalid configuration was supplied'));
+      console.log(ajv.errors);
+      process.exit(1);
+    }
+    console.log('Found configuration', result.config);
+    return result.config;
+  }
+}
